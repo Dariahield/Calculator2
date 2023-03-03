a7576df3d59d86fec17ef5980709bb95331ced67
MONSTER B <dvnv36@mail.ru>
2023-03-03 17:10:43 +0300

    Calculator2HomeworkDemoApplication

---
+++ src/main/java/calculator2/calculator2homeworkdemo/Calculator2HomeworkDemoApplication.java
@@ -0,0 +1,13 @@
+package calculator2.calculator2homeworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator2HomeworkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator2HomeworkDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/calculator2/calculator2homeworkdemo/Calculator2HomeworkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package calculator2.calculator2homeworkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator2HomeworkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
